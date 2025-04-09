1b1936bc365b3db1b26b85636f67c1a00db55f76
Haseeb Islam <haseeb.islam22@gmail.com>
2025-04-09 10:45:31 +0500

    projects module impemented

--- src/api/custom_controller/routes/global.js
+++ src/api/custom_controller/routes/global.js
@@ -8,45 +8,7 @@ module.exports = {
         auth: false,
       },
 
-      // add query parameters
-
-      query: {
-        populate: {
-          type: "string",
-          default: "*",
-        },
-      },
-
-      description: "Fetch combined data from all collections",
-      tags: ["api::global.global"],
-      // Define the response schema
-      response: {
-        200: {
-          description: "Combined data from all collections",
-          content: {
-            "application/json": {
-              schema: {
-                type: "object",
-                additionalProperties: true,
-              },
-            },
-          },
-        },
-        500: {
-          description: "Internal server error",
-          content: {
-            "application/json": {
-              schema: {
-                type: "object",
-                properties: {
-                  error: { type: "string" },
-                  details: { type: "string" },
-                },
-              },
-            },
-          },
-        },
-      },
+      // Define the parameters for the endpoint
     },
   ],
 };
